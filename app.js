900d37bb221f0c546c1cf6b3c77c777c4a64ef8f
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-09-13 17:26:14 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+     var maths;
+     maths = Number(prompt("Enter your Maths marks: "));
+     var computer;
+     computer = Number(prompt("Enter your computer marks: "));
+     var physics;
+     physics = Number(prompt("Enter your physics marks: "));
+     var english;
+     english = Number(prompt("Enter your english marks: "));
+     var sst;
+     sst = Number(prompt("Enter your sst marks: "));
+
+     var totalmarks = 500
+     obtmarks = maths + computer + physics + english + sst;
+     
+     
+     alert("Total Marks Are " + obtmarks ) 
+     
+     var percentage = (obtmarks / totalmarks) *100; 
+
+
+
+        if (percentage<=100 && percentage>=80){
+            alert("A1 grade");
+        }
+        else if (percentage<=79 && percentage>=70){
+            alert("A grade");
+        }
+        else if (percentage<=69 && percentage>=60){
+            alert("B grade");
+        }
+        else if (percentage<=59 && percentage>=50){
+            alert("C grade");
+        }
+        else if (percentage<=49 && percentage>=40){
+            alert("D grade");
+        }
+        else if (percentage<40){
+            alert("Fail");
+        }
+        else{
+            alert("Wrong Input")
+        }
